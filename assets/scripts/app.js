f12eb8eeb16382d9fbb5120c6bb3bdb841d7aafb
YevaMV <eva280988@gmail.com>
2022-01-17 01:41:07 +0400

    GET request

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+const listElem = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+xhr.onload = function () {
+    const listOfPosts = JSON.parse(xhr.response);
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElem.append(postEl);
+    }
+}
+xhr.send();
+
+
+
