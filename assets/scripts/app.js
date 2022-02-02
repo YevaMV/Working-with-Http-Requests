03921623299024055ded5b3afb08ff1e75e0f954
YevaMV <eva280988@gmail.com>
2022-02-03 01:44:10 +0400

    updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,8 +5,10 @@ const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
 
+xhr.responseType = 'json';
+
 xhr.onload = function () {
-    const listOfPosts = JSON.parse(xhr.response);
+    const listOfPosts = xhr.response;
     for (const post of listOfPosts) {
         const postEl = document.importNode(postTemplate.content, true);
         postEl.querySelector('h2').textContent = post.title.toUpperCase();
